dd5677ab57507d43e26f293eaac83f9ab834e05b
Michael Street <mi.street@gmail.com>
2019-05-17 16:00:52 -0400

    Updates

---
+++ precache-manifest.2289e165d45a8b8dc63f6808343fb1f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1231a54c30be5e6b13abfb2fff66c4e2",
+    "url": "/collect-emails/index.html"
+  },
+  {
+    "revision": "57f7d50f6eb45378ea5c",
+    "url": "/collect-emails/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "71525d56024f849e646a",
+    "url": "/collect-emails/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "57f7d50f6eb45378ea5c",
+    "url": "/collect-emails/static/js/2.c1711b0c.chunk.js"
+  },
+  {
+    "revision": "71525d56024f849e646a",
+    "url": "/collect-emails/static/js/main.2e6aef10.chunk.js"
+  },
+  {
+    "revision": "3233a26ab76721b80855",
+    "url": "/collect-emails/static/js/runtime~main.22f2bdd8.js"
+  }
+]);
