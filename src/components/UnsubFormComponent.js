ebb8f646f63505a586ad9592261f9cf4fa46351c
Michael Street <mi.street@gmail.com>
2019-05-18 10:30:34 -0400

    switch submission type for unsub

--- src/components/UnsubFormComponent.js
+++ src/components/UnsubFormComponent.js
@@ -19,39 +19,34 @@ class UnsubForm extends Component {
   }
 
   submitEmail() {
-    const options = {
-      method: 'POST',
-      uri: 'https://crm.zoho.com/crm/Unsubscribe',
-      encoding: 'UTF-8',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Upgrade-Insecure-Requests': 1
-      },
-      form: {
-        xnQsjsdp:
-          '471f3021ed654f3e4892f442fdd9c42e02dc17ac2d8e5198b73b2b5f87cee4fa',
-        actionType: 'dW5zdWJzY3JpYmU=',
-        returnURL: 'https://mstreet3.github.io/collect-emails/',
-        email: this.state.email
-      }
-    };
-
-    return rp(options)
-      .then((response) => {
-        console.log('Response went through!');
-        console.log(`Response is: ${JSON.stringify(response)}`);
-      })
-      .catch((err) => console.log(err))
-      .finally(() =>
-        this.setState({
-          email: ''
-        })
-      );
+    this.setState({ email: '' });
   }
 
   render() {
     return (
-      <form onSubmit={this.submitEmail}>
+      <form
+        action="https://crm.zoho.com/crm/Unsubscribe?encoding=UTF-8"
+        method="POST"
+        name="unsubForm"
+        onSubmit={this.submitEmail}
+      >
+        <div className="form-group">
+          <input
+            className="d-none"
+            name="xnQsjsdp"
+            value="471f3021ed654f3e4892f442fdd9c42e02dc17ac2d8e5198b73b2b5f87cee4fa"
+          />
+          <input
+            className="d-none"
+            name="actionType"
+            value="dW5zdWJzY3JpYmU="
+          />
+          <input
+            className="d-none"
+            name="returnURL"
+            value="https://mstreet3.github.io/collect-emails"
+          />
+        </div>
         <div className="input-group mb-3">
           <label htmlFor="unsubEmail" className="sr-only">
             Email
